0ca2341d39d35606236a04ae019897f6cd92b014
qazal <qazal.software@gmail.com>
2024-04-08 20:51:31 +0300

    support GET

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,18 +1,13 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import Base from "./Base";
+import { API_URL } from "./utils";
 
 const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
       queryFn: async ({ queryKey }) => {
-        if (process.env.REACT_APP_API_URL == null) {
-          throw new Error("No BASE_URL found");
-        }
-        const res = await fetch(
-          (process.env.REACT_APP_API_URL + queryKey[0]) as string,
-        );
-        const data = await res.json();
-        return data;
+        const res = await fetch((API_URL + queryKey[0]) as string);
+        return await res.json();
       },
     },
   },
--- server/api/index.py
+++ server/api/index.py
@@ -1,13 +1,7 @@
-import json, functools, pickle, io, importlib, cgi, os, redis, uuid
+import json, os, redis
 from urllib.parse import urlparse, parse_qs
 from typing import cast
 from http.server import BaseHTTPRequestHandler
-from tinygrad.codegen.linearizer import Linearizer
-
-from tinygrad.helpers import to_function_name
-from tinygrad.ops import LazyOp, LoadOps
-from tinygrad.renderer.cstyle import OpenCLRenderer
-from tinygrad.features.graph import _tree
 
 class handler(BaseHTTPRequestHandler):
   def _set_headers(self):
@@ -18,18 +12,7 @@ class handler(BaseHTTPRequestHandler):
     self.send_header('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type')
     self.end_headers()
 
-  def do_OPTIONS(self):
-    self._set_headers()
-
-  def do_PUT(self):
-    content_length = int(self.headers['Content-Length'])
-    data = json.loads(self.rfile.read(content_length))
-    assert_valid(data)
-    r = redis.Redis(host=getenv("REDIS_HOST"), port=33331, password=getenv("REDIS_PASSWORD"), ssl=True)
-    r.set(share_id:=uuid.uuid4().hex, json.dumps(data, indent=None))
-    self._set_headers()
-    self.end_headers()
-    return self.wfile.write(json.dumps({ "id": share_id }, indent=None).encode('utf-8'))
+  def do_OPTIONS(self): self._set_headers()
 
   def do_GET(self):
     self._set_headers()
@@ -42,55 +25,3 @@ class handler(BaseHTTPRequestHandler):
 def getenv(name: str):
   assert (val:=os.getenv(name))
   return val
-
-class TinyUnpickler(pickle.Unpickler):
-  def find_class(self, module: str, name: str):
-    assert module in ["tinygrad.ops", "tinygrad.dtype", "tinygrad.shape.shapetracker", "tinygrad.shape.view"] \
-        or (module == "tinygrad.tensor" and name == "custom_random"), f"forbidden object {module} {name}"
-    return getattr(importlib.import_module(module), name)
-
-def tiny_load(s): return TinyUnpickler(io.BytesIO(s)).load()
-
-def transform_node(src):
-  node = {"id": src["id"], "inputs": src["inputs"], "outputs": src["outputs"]}
-  if src["ast"][0].op not in LoadOps:
-    lin = cached_linearize(*src["ast"])
-    name = to_function_name(lin.name)
-    node["fill"] = "red" if name.startswith("r") else "blue"
-    node["code"] = OpenCLRenderer(name, lin.uops)
-    node["label"] = name
-    node["shape"] = str(src["ast"][0].arg.st.shape)
-    node["ast"] = "\n".join(["\n".join([f"{str(i).rjust(3)} {s}" for i,s in enumerate(_tree(op, {}, [-1]))]) for op in src["ast"]])
-
-  else:
-    node["fill"] = "white"
-    node["code"], node["shape"] = "", ""
-    node["label"] = str(src["ast"][0].op)
-  return node
-
-def assert_valid(data):
-  assert "nodes" in data and "edges" in data and len(data.keys()) == 2
-  required, optional = {"id", "inputs", "outputs", "fill", "label", "shape"}, {"code", "ast"}
-  assert isinstance(data["nodes"], list) and isinstance(data["edges"], list)
-  for node in data["nodes"]:
-    assert isinstance(node, dict)
-    assert not required - (fields:=set(node.keys())) and not fields - required - optional, f"got {set(node.keys())}"
-    assert isinstance(node["id"], str)
-    assert isinstance(node["inputs"], list)
-    assert isinstance(node["outputs"], list)
-    assert isinstance(node["fill"], str)
-    assert isinstance(node["label"], str)
-    assert isinstance(node["shape"], str)
-    if "ast" in node: assert isinstance(node["ast"], str)
-    if "code" in node: assert isinstance(node["code"], str)
-  required = {"source", "target", "id", "label"}
-  for edge in data["edges"]:
-    assert isinstance(edge, dict)
-    assert not required - (fields:=set(edge.keys())) and not fields - required
-    for k in required: assert isinstance(edge[k], str)
-
-@functools.lru_cache(None)
-def cached_linearize(*ast:LazyOp) -> Linearizer:
-  lin= Linearizer(*ast)
-  lin.linearize()
-  return lin
