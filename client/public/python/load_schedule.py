1fea3d68f4206ba682d772ac013b5fdefc4a06c1
qazal <qazal.software@gmail.com>
2024-05-18 14:47:45 +0300

    update to master

--- client/public/python/load_schedule.py
+++ client/public/python/load_schedule.py
@@ -1,11 +1,12 @@
 import functools, re, pickle, importlib, io, json, random
-from typing import Dict
-from tinygrad.ops import List, ScheduleItem, LazyOp, LoadOps
+from typing import Dict, List
+from tinygrad.ops import LazyOp, LoadOps
+from tinygrad.engine.schedule import ScheduleItem
 from tinygrad.codegen.linearizer import Linearizer
 from tinygrad.helpers import to_function_name
 from tinygrad.renderer.cstyle import OpenCLRenderer
-from tinygrad.features.graph import _tree
-from tinygrad.device import CompilerOptions
+from tinygrad.engine.graph import _tree
+from tinygrad.renderer import Renderer
 
 class Buffer:
   def __init__(self, device:str, size:int, dtype, opaque=None, options=None, initial_value=None, lb_refcount=0) -> None:
@@ -17,7 +18,7 @@ class Buffer:
 
 @functools.lru_cache(None)
 def cached_linearize(*ast:LazyOp) -> Linearizer:
-  lin= Linearizer(*ast, opts=CompilerOptions("GPU"))
+  lin= Linearizer(*ast, opts=Renderer())
   lin.linearize()
   return lin
 
@@ -29,7 +30,7 @@ def transform_node(src):
       lin = cached_linearize(*src["ast"])
       name = to_function_name(lin.name)
       node["fill"] = "green" if bool(re.search(r'r\d', name)) else "red" if name.startswith("r") else "green" if bool(re.search(r'E\d', name)) else "yellow" if "ASSIGN" in str(src["outputs"]) else "blue"
-      node["code"] = OpenCLRenderer(name, lin.uops)
+      node["code"] = OpenCLRenderer().render(name, lin.uops)
       node["label"] = name
     except Exception as e:
       print("FAILED TO LINEARIZE", e, src["ast"])
@@ -54,7 +55,7 @@ def _parse(gi: int, i:int, si): return transform_node({ 'id': f"{gi}-{str(i)}", 
 nodes, edges = [], []
 class TinyUnpickler(pickle.Unpickler):
   def find_class(self, module: str, name: str):
-    if module == "tinygrad.buffer" and name == "Buffer": return Buffer
+    if module == "tinygrad.device" and name == "Buffer": return Buffer
     return getattr(importlib.import_module(module), name)
 with open("/sched.pkl", "rb") as f: s = f.read()
 data = TinyUnpickler(io.BytesIO(s)).load()
