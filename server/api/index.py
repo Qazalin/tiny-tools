fba787c16df9b0d2717d5fda94d196a768bfe524
qazal <qazal.software@gmail.com>
2024-07-26 20:40:14 +0300

    simpler api

--- server/api/index.py
+++ server/api/index.py
@@ -1,8 +1,7 @@
-import json, os, redis, subprocess
+import json, os, redis
 from urllib.parse import urlparse, parse_qs
 from typing import cast
 from http.server import BaseHTTPRequestHandler
-from tinygrad.runtime.support.hip_comgr import compile_hip
 
 class handler(BaseHTTPRequestHandler):
   def _set_headers(self):
@@ -22,19 +21,6 @@ class handler(BaseHTTPRequestHandler):
     if query_id is None: return self.wfile.write(json.dumps({ "ok": True }, indent=None).encode('utf-8'))
     r = redis.Redis(host=getenv("REDIS_HOST"), port=33331, password=getenv("REDIS_PASSWORD"), ssl=True)
     return self.wfile.write(cast(bytes, r.get(query_id)))
-
-  def do_POST(self):
-    content_length = int(self.headers['Content-Length'])
-    post_data = self.rfile.read(content_length)
-    parsed_data = parse_qs(post_data.decode('utf-8'))
-    code = parsed_data.get('code', [''])[0]
-    lib = compile_hip(code)
-    asm = subprocess.check_output(["/opt/rocm/llvm/bin/llvm-objdump", '-d', '-'], input=lib)
-    asm = '\n'.join([x for x in asm.decode('utf-8').split("\n") if 's_code_end' not in x])
-    self.send_response(200)
-    self.send_header("Content-type", "text/plain")
-    self.end_headers()
-    self.wfile.write(asm.encode())
 
 def getenv(name: str):
   assert (val:=os.getenv(name))
