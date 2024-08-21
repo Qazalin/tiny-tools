5d4e14b5e3f5b48ae75bf4b5601aeaa59f3cfd72
qazal <qazal.software@gmail.com>
2024-08-21 17:41:52 +0300

    seed 2

--- server/api/seed.py
+++ server/api/seed.py
@@ -11,6 +11,7 @@ SYSTEMS_MAP = {'Speed (AMD)': 'amd', 'Speed (AMD Training)': 'amd-train', 'Speed
 SYSTEMS = list(SYSTEMS_MAP.values())
 
 def get_runs(branch:str, per_page:int):
+  print(f"*** getting {per_page} runs for {branch}")
   res = requests.get(f"{BASE_URL}/actions/workflows/benchmark.yml/runs?branch={branch}&status=success&per_page={per_page}", headers=GH_HEADERS)
   assert res.status_code == 200, f"GET failed {res.status_code} {res.json()}"
   runs = res.json()["workflow_runs"]
@@ -22,11 +23,13 @@ def get_runs(branch:str, per_page:int):
       with io.BytesIO(res.content) as zip_content:
         with zipfile.ZipFile(zip_content, "r") as zip_ref: zip_ref.extractall(f"/tmp/benchmarks/{run['run_number']}_{run['head_sha']}/{SYSTEMS_MAP[artifact['name']]}")
 
-#get_runs("master", 100)
+if not os.getenv("CACHE_MASTER"): get_runs("master", 10)
+get_runs("update_benchmark", 1)
 files = list(sorted(list(glob.glob(f"/tmp/benchmarks/*")), key=lambda f:int(f.split("/")[-1].split("_")[0])))
 commits = [f.split("/")[-1] for f in files]
 ret = []
 for file in TRACKED_BENCHMARKS:
+  print(f"*** extracting {file}")
   regex, systems, skip_count, max_count = TRACKED_BENCHMARKS[file]
   benchmarks = [[] for _ in range(len(SYSTEMS))]
   for system in systems:
@@ -41,9 +44,12 @@ for file in TRACKED_BENCHMARKS:
 commits_ret = [{"sha": x.split("_")[1]} for x in commits]
 db_url = urlparse(os.environ["DB_URL"])
 conn = psycopg2.connect(database=db_url.path[1:], user=db_url.username, password=db_url.password, host=db_url.hostname, port=db_url.port)
+print(f"*** uploading {len(commits_ret)} commits")
 with conn.cursor() as cursor:
-  cursor.execute("""CREATE TABLE IF NOT EXISTS benchmark ( id SERIAL PRIMARY KEY, benchmarks TEXT, filename TEXT, system TEXT)""")
-  cursor.execute("""CREATE TABLE IF NOT EXISTS commits (id SERIAL PRIMARY KEY, sha TEXT)""")
+  cursor.execute("""DROP TABLE IF EXISTS benchmark""")
+  cursor.execute("""DROP TABLE IF EXISTS commits""")
+  cursor.execute("""CREATE TABLE benchmark ( id SERIAL PRIMARY KEY, benchmarks TEXT, filename TEXT, system TEXT)""")
+  cursor.execute("""CREATE TABLE commits (id SERIAL PRIMARY KEY, sha TEXT)""")
   for item in ret: cursor.execute("INSERT INTO benchmark (benchmarks, filename, system) VALUES (%s, %s, %s)", (json.dumps(item["benchmarks"]), item["filename"], item["system"]))
   for commit in commits_ret: cursor.execute("INSERT INTO commits (sha) VALUES (%s)", (commit["sha"],))
 conn.commit()
