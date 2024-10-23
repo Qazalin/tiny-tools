57ad52f0bf24a19f17290598654f1095fa502b2c
qazal <qazal.software@gmail.com>
2024-10-23 17:13:24 +0300

    delete client

--- client/src/uop-graph/index.tsx
+++
@@ -1,5 +0,0 @@
-import { GraphBatch, UOpNode } from "../types";
-
-export default function UOpGraph({ batch }: { batch: GraphBatch<UOpNode> }) {
-  return <div>hello from me!</div>;
-}
--- upload_graph.py
+++
@@ -1,8 +0,0 @@
-import redis, datetime, json, zlib
-from server.api.index import getenv
-
-r = redis.Redis(host=getenv("REDIS_HOST"), port=6379, password=getenv("REDIS_PASSWORD"), ssl=True)
-id = int(datetime.datetime.now().timestamp())
-dump = json.dumps(json.load(open("/tmp/graph.json")))
-r.set(str(id), zlib.compress(dump.encode("utf-8")))
-print(id)
